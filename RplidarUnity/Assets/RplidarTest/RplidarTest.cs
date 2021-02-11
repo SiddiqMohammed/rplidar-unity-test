83a7de2ac6676107da51c5871a1afb6e399e30dc
siddiq <msm2029@gmail.com>
2021-02-11 16:21:10 +0400

    updated the code and hardcoded touchpoints

--- RplidarUnity/Assets/RplidarTest/RplidarTest.cs
+++ RplidarUnity/Assets/RplidarTest/RplidarTest.cs
@@ -9,14 +9,22 @@ public class RplidarTest : MonoBehaviour
     List<int> angles_list_0 = new List<int>();
     List<int> angles_list_1 = new List<int>();
     List<int> angles_list_2 = new List<int>();
+    List<int> angles_list_3 = new List<int>();
+    List<int> angles_list_4 = new List<int>();
+    List<int> angles_list_5 = new List<int>();
+    List<int> angles_list_6 = new List<int>();
+    List<int> angles_list_7 = new List<int>();
+    List<int> angles_list_8 = new List<int>();
+    List<int> angles_list_9 = new List<int>();
 
 
     // distances of each touchpoint (in mm)
-    int[] tp_distances = { 700, 700, 700 };
-    int[] tp_angles = { 210, 330, 270 };
+    // Left to Right
+    int[] tp_distances = { 1720, 1560, 1200, 900, 1210, 1410, 1520, 1710, 1450, 1160 };
+    int[] tp_angles = { 54, 40, 27, 27, 9, 352, 334, 324, 315, 300 };
 
-    double[] filtered_val = { 0, 0, 0 };
-    int[] filter_counter = { 0, 0, 0 };
+    double[] filtered_val = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    int[] filter_counter = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
     // redundant true values to avoid spikes being registered as true
     int rdc_val = 2;
@@ -24,9 +32,9 @@ public class RplidarTest : MonoBehaviour
     // number of times the value has to be repeated to be registered as precise
     int dist_precision = 5;
 
-    // tolerances for the distance gives
-    int tp_dist_tolerance = 10;
-    int tp_angle_tolerance = 2;
+    // tolerances for the distance and angle
+    int tp_dist_tolerance = 110;
+    int tp_angle_tolerance = 5;
 
     public string port = "COM8";
 
@@ -53,6 +61,21 @@ public class RplidarTest : MonoBehaviour
     void Update()
     {
         getAllData();
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (string.IsNullOrEmpty(port))
+            {
+                return;
+            }
+
+            RplidarBinding.OnConnect(port);
+            RplidarBinding.StartMotor();
+            RplidarBinding.StartScan();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            RplidarBinding.OnDisconnect();
+        }
     }
 
     private void OnGUI()
@@ -148,44 +171,43 @@ public class RplidarTest : MonoBehaviour
             for (int i = 0; i < count; i++)
             {
 
-                // if (data[i].theta < 271 && data[i].theta > 269)
+                // for (int j = 0; j < count; j++)
                 // {
-                //     print("DIST " + data[i].distant);
+
                 // }
 
-                if ((data[i].theta > tp_angles[0] - tp_angle_tolerance) && (data[i].theta < tp_angles[0] + tp_angle_tolerance))
-                {
-                    angles_list_0.Add((int)data[i].distant);
-                    // print("distance: " +data[i].distant);
+                // if ((data[i].theta > tp_angles[0] - tp_angle_tolerance) && (data[i].theta < tp_angles[0] + tp_angle_tolerance))
+                // {
+                //     angles_list_0.Add((int)data[i].distant);
+                //     // print("distance: " +data[i].distant);
 
-                    if (angles_list_0.Count > dist_precision)
-                    {
-                        float avg_val = average(angles_list_0);
-                        filtered_val[0] = (0.8 * filtered_val[0]) + (0.2 * avg_val);
+                //     if (angles_list_0.Count > dist_precision)
+                //     {
+                //         float avg_val = average(angles_list_0);
+                //         filtered_val[0] = (0.8 * filtered_val[0]) + (0.2 * avg_val);
 
-                        if ((filtered_val[0] > tp_distances[0] - tp_dist_tolerance) && (filtered_val[0] < tp_distances[0] + tp_dist_tolerance))
-                        {
+                //         if ((filtered_val[0] > tp_distances[0] - tp_dist_tolerance) && (filtered_val[0] < tp_distances[0] + tp_dist_tolerance))
+                //         {
+                //             filter_counter[0]++;
 
-                            filter_counter[0]++;
-
-                            if (filter_counter[0] > rdc_val)
-                            {
-                                button_clicked = 0;
-                                print("0 at " + filtered_val[0]);
-                            }
-                        }
-                        else
-                        {
-                            filter_counter[0] = 0;
-                        }
-                        angles_list_0.Clear();
-                    }
-
-                }
+                //             if (filter_counter[0] > rdc_val)
+                //             {
+                //                 button_clicked = 0;
+                //                 print("0 at " + filtered_val[0]);
+                //             }
+                //         }
+                //         else
+                //         {
+                //             filter_counter[0] = 0;
+                //         }
+                //         angles_list_0.Clear();
+                //     }
+                // }
 
                 if ((data[i].theta > tp_angles[1] - tp_angle_tolerance) && (data[i].theta < tp_angles[1] + tp_angle_tolerance))
                 {
                     angles_list_1.Add((int)data[i].distant);
+                    print("data 1 " + data[i].distant);
 
                     if (angles_list_1.Count > dist_precision)
                     {
